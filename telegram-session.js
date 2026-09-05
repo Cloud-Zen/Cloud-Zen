6870a532075d0dd5d3e232c8f867f24c5b7cc2c3
Cloud-Zen <bon66004@gmail.com>
2026-09-05 14:32:39 +0530

    Update telegram-session.js

--- telegram-session.js
+++ telegram-session.js
@@ -1,5 +1,4 @@
-"use strict";
-
+  "use strict";
 /* Run locally, not on Render, to create the Telegram MTProto session string. */
 const readline = require("node:readline/promises");
 const { stdin: input, stdout: output } = require("node:process");
@@ -39,4 +38,4 @@ const { stdin: input, stdout: output } = require("node:process");
   console.error(error?.stack || error);
   process.exit(1);
 });
-                  
+
